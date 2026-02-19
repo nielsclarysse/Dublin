4b89af2e976a7cff8bbc7edecb388bc25fe4d062
Maurice De Kegel <199232726+MriceDK@users.noreply.github.com>
2026-02-19 15:52:23 +0100

    Adds navigation

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+function init() {
+    document.querySelector(".nav-blog").addEventListener("click", showBlog);
+    document.querySelectorAll(".nav-home").forEach(el => el.addEventListener("click", showHome));
+
+}
+
+function showBlog() {
+    document.querySelector(".home").classList.add("hidden");
+    document.querySelector(".blog").classList.remove("hidden");
+}
+
+function showHome() {
+    document.querySelector(".blog").classList.add("hidden");
+    document.querySelector(".home").classList.remove("hidden");
+}
+
+init();
